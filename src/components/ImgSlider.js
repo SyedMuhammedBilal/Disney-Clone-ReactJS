8690efa123a348ec93f0d36bbca46763b34f5017
Syed Muhammed Bilal <intergo878@gmail.com>
2021-05-05 23:00:02 +0500

    coursal added

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -18,7 +18,7 @@ const Carousel = styled(Slider)`
         overflow: visible;
     }
 
-    li.slick-active button::before {
+    li.slick-active button:before {
         color: #fff;
     }
 
@@ -29,6 +29,7 @@ const Carousel = styled(Slider)`
 `
 
 const Wrap = styled.div`
+    cursor: pointer;
     img {
         width: 100%;
         height: 100%;
@@ -36,6 +37,11 @@ const Wrap = styled.div`
         box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
                     rgb(0 0 0 / 73%) 0px 16px 10px -10px;  
         border: 4px solid transparent;
+        transition-duration: 300ms;
+
+        &:hover {
+            border: 4px solid rgba(249, 249, 249, 0.8);
+        }
     }
 
 
